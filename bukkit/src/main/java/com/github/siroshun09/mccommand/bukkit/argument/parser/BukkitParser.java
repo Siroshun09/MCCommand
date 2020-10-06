bdb955a536a68957e1ec7585a7d1f932477719c3
Siroshun09 <siro.shun8@gmail.com>
2020-10-06 20:46:16 +0900

    [Add] BukkitParser, BungeeParser

---
+++ bukkit/src/main/java/com/github/siroshun09/mccommand/bukkit/argument/parser/BukkitParser.java
@@ -0,0 +1,58 @@
+/*
+ *     Copyright 2020 Siroshun09
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package com.github.siroshun09.mccommand.bukkit.argument.parser;
+
+import com.github.siroshun09.mccommand.common.argument.parser.ArgumentParser;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+
+/**
+ * Parsers for Bukkit.
+ */
+public final class BukkitParser {
+
+    private BukkitParser() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * An instance of {@link ArgumentParser} that parses an {@link com.github.siroshun09.mccommand.common.argument.Argument} to {@link Material}
+     */
+    public static final ArgumentParser<Material> MATERIAL = new MaterialParser();
+
+    /**
+     * An instance of {@link ArgumentParser} that parses an {@link com.github.siroshun09.mccommand.common.argument.Argument} to {@link Player}
+     */
+    public static final ArgumentParser<Player> PLAYER = new PlayerParser();
+
+    /**
+     * An instance of {@link ArgumentParser} that parses an {@link com.github.siroshun09.mccommand.common.argument.Argument} to {@link Player}
+     */
+    public static final ArgumentParser<Player> PLAYER_EXACT = new PlayerExactParser();
+
+    /**
+     * An instance of {@link ArgumentParser} that parses an {@link com.github.siroshun09.mccommand.common.argument.Argument} to {@link OfflinePlayer}
+     */
+    public static final ArgumentParser<OfflinePlayer> OFFLINE_PLAYER = new OfflinePlayerParser();
+
+    /**
+     * An instance of {@link ArgumentParser} that parses an {@link com.github.siroshun09.mccommand.common.argument.Argument} to {@link Sound}
+     */
+    public static final ArgumentParser<Sound> SOUND = new SoundParser();
+}
