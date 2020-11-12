822adb32674b011ab7e36641db7a35071ecc6037
Siroshun09 <siro.shun8@gmail.com>
2020-11-12 09:46:51 +0900

    [Add] BungeeCommandFactory#registerAsync

--- bungee/src/main/java/com/github/siroshun09/mccommand/bungee/BungeeCommandFactory.java
+++ bungee/src/main/java/com/github/siroshun09/mccommand/bungee/BungeeCommandFactory.java
@@ -27,6 +27,9 @@ import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.TabExecutor;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 /**
  * The class for registering commands to BungeeCord.
  */
@@ -46,6 +49,39 @@ public final class BungeeCommandFactory {
         ProxyServer.getInstance().getPluginManager().registerCommand(plugin, new BungeeCommandImpl(command));
     }
 
+    /**
+     * Registers the command.
+     * <p>
+     * If you register a command with this method, it will be executed asynchronously.
+     * <p>
+     * Note:
+     * <p>
+     * The tab completion will execute on main thread.
+     *
+     * @param plugin  the BungeeCord plugin
+     * @param command the command to register
+     */
+    public static void registerAsync(@NotNull Plugin plugin, @NotNull Command command) {
+        registerAsync(plugin, command, Executors.newSingleThreadExecutor());
+    }
+
+    /**
+     * Registers the command.
+     * <p>
+     * If you register a command with this method, it will be executed asynchronously.
+     * <p>
+     * Note:
+     * <p>
+     * The tab completion will execute on main thread.
+     *
+     * @param plugin   the BungeeCord plugin
+     * @param command  the command to register
+     * @param executor the executor to run command
+     */
+    public static void registerAsync(@NotNull Plugin plugin, @NotNull Command command, @NotNull Executor executor) {
+        ProxyServer.getInstance().getPluginManager().registerCommand(plugin, new AsyncBungeeCommandImpl(command, executor));
+    }
+
     private static class BungeeCommandImpl extends net.md_5.bungee.api.plugin.Command implements TabExecutor {
 
         private final Command command;
@@ -59,6 +95,43 @@ public final class BungeeCommandFactory {
         public void execute(CommandSender sender, String[] args) {
             command.onExecution(
                     createContext(sender, args)
+            );
+        }
+
+        @Override
+        public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
+            return command.onTabCompletion(
+                    createContext(sender, args)
+            );
+        }
+
+        private CommandContext createContext(CommandSender sender, String[] args) {
+            return SimpleCommandContext.newBuilder()
+                    .setCommand(command)
+                    .setSender(new BungeeSender(sender))
+                    .setArguments(args)
+                    .setLabel(command.getName())
+                    .build();
+        }
+    }
+
+    private static class AsyncBungeeCommandImpl extends net.md_5.bungee.api.plugin.Command implements TabExecutor {
+
+        private final Command command;
+        private final Executor executor;
+
+        private AsyncBungeeCommandImpl(@NotNull Command command, @NotNull Executor executor) {
+            super(command.getName(), null, command.getAliases().toArray(new String[0]));
+            this.command = command;
+            this.executor = executor;
+        }
+
+        @Override
+        public void execute(CommandSender sender, String[] args) {
+            executor.execute(() ->
+                    command.onExecution(
+                            createContext(sender, args)
+                    )
             );
         }
 
